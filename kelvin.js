05f6918b418a0d0df7ac5947c635d9ef3d27ce1c
Onur Ozfuttu <69537532+onurozfuttu@users.noreply.github.com>
2022-11-08 14:05:14 +0300

    Delete kelvin.js

--- kelvin.js
+++
@@ -1,7 +0,0 @@
-let kelvin = 293;
-// this is kelvin
-let celsius = kelvin - 273;
-// celsius is 273 degrees less than kelvin
-// Fahrenheit = Celsius * (9/5) + 32
-let fahrenheit = Math.floor(celsius * (9/5) + 32);
-console.log(`The temperature is ${fahrenheit} degrees fahrenheit.`);
