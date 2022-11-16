df2c01cc0d5cbb1e3964efa27d9c35acbf54a20b
Onur Ozfuttu <69537532+onurozfuttu@users.noreply.github.com>
2022-11-16 18:41:21 +0300

    JavaScript Syntax, Part 2 Challenge - Greet Aliens
    Greet Aliens

---
+++ building_interactive_websites/part2_code_challenges/greetAliens.js
@@ -0,0 +1,20 @@
+// Write your code here:
+
+const greetAliens = arrName => {
+    for(i=0;i<arrName.length;i++){
+      console.log(`Oh powerful ${arrName[i]}, we humans offer our unconditional surrender!`);
+    }
+  }
+  
+  
+  
+  
+  
+  
+  // When you're ready to test your code, uncomment the below and run:
+  
+  const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+  
+  greetAliens(aliens);
+  
+  
