dea0f9ed89430d7c120b8a849e33f1f11d80d456
Onur Ozfuttu <69537532+onurozfuttu@users.noreply.github.com>
2022-11-30 08:19:06 +0300

    React, Part 1 - Animal Fun Facts
    Animal Fun Facts

---
+++ Front-end-development/animalFunFacts.js
@@ -0,0 +1,43 @@
+import { animals } from './animals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = '';
+const background = <img className='background' alt='ocean' src='/images/ocean.jpg' />;
+const images = [];
+
+for(const animal in animals){
+  images.push(
+   <img
+    key={animal}
+    className='animal'
+    alt={animal}
+    src={animals[animal].image}
+    ariaLabel= {animal}
+    role= 'button'
+    onClick={displayFact}
+   />
+  )
+}
+const animalFacts = (
+<div>
+<h1>
+{title === '' ? 'Click an animal for a fun fact': title}
+</h1>
+{background}
+<div className='animals'>
+{images}
+</div>
+<p id='fact'></p>
+</div>
+);
+
+function displayFact(e) {
+const selectedAnimal = e.target.alt;
+const animalInfo = animals[selectedAnimal];
+const randomFacts = Math.floor(Math.random() * animalInfo.facts.length);
+const lastFacts = animalInfo.facts[randomFacts];
+document.getElementById('fact').innerHTML = lastFacts;
+}
+
+ReactDOM.render(animalFacts, document.getElementById('root'));
